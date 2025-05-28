8c04602e1039d0659bf5b1607467d484da7f1fc9
jalis <jalismahamud31@gmail.com>
2025-05-28 18:28:32 +0600

    Update

--- app/Http/Controllers/Api/Backend/ApiContentController.php
+++ app/Http/Controllers/Api/Backend/ApiContentController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Api\Backend;
 
+use Exception;
 use App\Models\Content;
 use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 
 class ApiContentController extends Controller
@@ -13,39 +15,60 @@ class ApiContentController extends Controller
 
     public function hathaYoga(Request $request)
     {
-        $query = Content::with(['category', 'contentType', 'contentDuration'])->where('category_id', 1);
+        try {
+            $query = Content::with(['category', 'contentType', 'contentDuration'])
+                ->where('category_id', 1);
 
-        dd($query);
+            if ($request->has('category')) {
+                $query->whereHas('category', function ($q) use ($request) {
+                    $q->where('title', $request->category);
+                });
+            }
 
+            if ($request->has('level')) {
+                $query->where('level', $request->level);
+            }
 
-        if ($request->has('category')) {
-            $query->whereHas('category', function ($q) use ($request) {
-                $q->where('title', $request->category);
-            });
+            if ($request->has('duration')) {
+                $query->whereHas('contentDuration', function ($q) use ($request) {
+                    $q->whereBetween('length', [
+                        (int) $request->duration,
+                    ]);
+                });
+            }
+
+            if ($request->has('goal')) {
+                $query->whereJsonContains('goals', $request->goal);
+            }
+
+            $results = $query->latest()->get();
+
+           
+            if ($results->isEmpty()) {
+                return $this->success([
+                    'category' => $request->category ?? 'Hatha Yoga',
+                    'contents' => []
+                ], 'No content found.', 200);
+            }
+
+          
+            $formatted = [
+                'category' => $results->first()->category->title ?? 'Hatha Yoga',
+                'contents' => $results->map(function ($item) {
+                    return [
+                        'title' => $item->title,
+                        'video' => $item->video_url ?? null, 
+                        'video_duration' => $item->contentDuration->length ?? null,
+                        'type' => $item->contentType->name ?? null
+                    ];
+                }),
+            ];
+
+            return $this->success($formatted, 'Content fetched successfully.', 200);
+        } catch (Exception $e) {
+
+            Log::info($e->getMessage());
+            return $this->error([], $e->getMessage(), 422);
         }
-
-
-        if ($request->has('level')) {
-            $query->where('level', $request->level);
-        }
-
-
-        if ($request->has('min_duration') && $request->has('max_duration')) {
-            $query->whereHas('contentDuration', function ($q) use ($request) {
-                $q->whereBetween('length', [
-                    (int) $request->min_duration,
-                    (int) $request->max_duration
-                ]);
-            });
-        }
-
-
-        if ($request->has('goal')) {
-            $query->whereJsonContains('goals', $request->goal);
-        }
-
-        $results = $query->latest()->get();
-
-        return $this->success($results, 'Content fetched successfully.', 200);
     }
 }
